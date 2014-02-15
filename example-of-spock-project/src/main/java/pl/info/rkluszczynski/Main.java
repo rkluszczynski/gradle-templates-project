5f6e14ea82507df9e6572c18e176e10b4dc23748
Rafal Kluszczynski <rkluszczynski@gmail.com>
2014-02-15 17:29:58 +0100

    example project with spock library

---
+++ example-of-spock-project/src/main/java/pl/info/rkluszczynski/Main.java
@@ -0,0 +1,9 @@
+package pl.info.rkluszczynski;
+
+public class Main {
+    
+    public static void main(String[] args) {
+	System.out.println("Hello, Java and Gradle!");
+    }
+    
+}
---
+++ example-of-spock-project/src/test/groovy/pl/info/rkluszczynski/HelloSpockTest.groovy
@@ -0,0 +1,20 @@
+
+import spock.lang.Specification
+
+/**
+ *
+ * @author Rafal
+ */
+class HelloSpockTest extends Specification {
+    
+    def "word length test"() {
+	expect:
+	    word.size() == length
+	where:
+	    word    | length
+	    'Hello' | 5
+	    'Spock' | 5
+    }
+	
+}
+
