03c93a85990cf0d8cfbea3d1281610c756fba9b0
Rafal Kluszczynski <klusi@icm.edu.pl>
2014-03-23 11:18:37 +0100

    minor changes

---
+++ example-of-database-access-h2/src/test/resources/init-data.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE areas (
+    ID INT,
+    Name VARCHAR(256)
+);
+
+INSERT INTO areas VALUES (1, 'food');
+
+
+--CREATE TABLE products IF NOT EXISTS {
+--}
+--
+--CREATE TABLE ingredients {
+--}
--- example-of-mvc-jsp-image-upload/src/main/java/pl/info/rkluszczynski/examples/upload/controller/HelloWorldController.java
+++ example-of-mvc-jsp-image-upload/src/main/java/pl/info/rkluszczynski/examples/upload/controller/HelloWorldController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
